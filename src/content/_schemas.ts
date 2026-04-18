84c7a8267b4c5286a1160b74638559f86399fb33
root <root@ubuntu4.tail75e52.ts.net>
2026-04-18 21:28:05 +0000

    sync: update public posts (2026-04-18)

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -11,6 +11,8 @@ export const blogSchema = z
     tags: z.array(z.string()).default(["others"]),
     ogImage: z.string().optional(),
     description: z.string(),
+    series: z.string().optional(),
+    seriesEntry: z.boolean().optional(),
   })
   .strict();
 
--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,6 +1,10 @@
 const getSortedPosts = (posts: any[]) =>
   posts
     .filter((post) => !post.draft)
+    .filter((post) => {
+      if (!post.series) return true;
+      return post.seriesEntry === true;
+    })
     .sort(
       (a, b) =>
         Math.floor(new Date(b.pubDatetime).getTime() / 1000) -
